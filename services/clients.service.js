597e0cc48d42fd44eb398a4192e1dca3f35ee0ab
jean-christophe-cartiaux <cartiaux.jc@hotmail.com>
2024-03-21 17:16:24 +0100

    Initialisation de la connection DB + les Services

--- services/clients.service.js
+++ services/clients.service.js
@@ -2,7 +2,18 @@ const sql=require('mssql');
 const sqlConfig=require('../database');
 
 const clientService={
-
+    getAll: async ()=> {
+        try{
+            await sql.connect(sqlConfig);
+            const Result = await sql.query('SELECT * FROM clients')
+            if(result){
+                return result.recordset;
+            }
+        }catch(err){
+            console.error(err)
+            res.sendStatus(500);
+        }
+    }
 }
 
-module.exports =    clientService;
+module.exports =clientService;
